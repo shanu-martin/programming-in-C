89888d33e42494848f93505765b6c36c0e49044f
shanu-martin <96837910+shanu-martin@users.noreply.github.com>
2022-01-05 01:11:31 +0530

    Add files via upload

---
+++ 1_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a[5],i;
+    printf("enter array elements\n");
+    for (i=0;i<=5;i++)
+    {
+      scanf("%d",&a[i]);
+
+
+    }
+    printf("The Array Elements are :\n");
+     for (i=0;i<=5;i++)
+    {
+      printf("%d\t",a[i]);
+
+
+    }
+    
+}
---
+++ 4_arrays_largest.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],n,i,l;
+    printf("enter the limit of array :\t");
+   scanf("%d",&n); 
+    printf("enter array elements\n");
+    for (i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+
+
+    }
+    printf("The Array Elements are :\n");
+     for (i=0;i<n;i++)
+    {
+      printf("%d\t",a[i]);
+
+
+    }
+    printf("\n");
+    l=a[0];
+    for(i=0;i<n;i++)
+    {
+      if(a[i]>l)
+      {
+        l=a[i];
+      }
+
+
+    }
+    printf("%d is largest",l);
+    
+}
