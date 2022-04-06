7979246437d582235ee485ecc791b619f164bd6f
shanu-martin <96837910+shanu-martin@users.noreply.github.com>
2022-04-06 20:39:01 +0530

    Add files via upload

---
+++ 6april1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+   int arr[10]={1,3,5,7,9,2,4,6,8,10};
+   int n=10;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
+   return 0;
+}
